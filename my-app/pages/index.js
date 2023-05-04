cd2e81b561634d046c7779f2cbc8546166abf0cc
Normal-case <8282chan94@gmail.com>
2023-05-04 23:24:20 +0900

    rendering image

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
 
 export default function Home() {
 
@@ -8,14 +9,24 @@ export default function Home() {
     fetch('https://jsonplaceholder.typicode.com/photos?_limit=10')
       .then(res => res.json())
       .then(json => {
-        console.log(json)
         setImages(json)
       })
   }, [])
-
   return (
     <div>
       Hello Next.js
+      {
+        images.map((image, index) => {
+          return (
+            <img
+              src={image.thumbnailUrl}
+              width={150}
+              height={150}
+            />
+          )
+        })
+      }
+
     </div>
   )
 }
