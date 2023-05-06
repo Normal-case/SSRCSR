0dd3e7111e62b142540d60d9ffbf0af318df7196
Normal-case <8282chan94@gmail.com>
2023-05-06 22:24:41 +0900

    apply css

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
@@ -9,24 +10,31 @@ export default function Home() {
     fetch('https://jsonplaceholder.typicode.com/photos?_limit=10')
       .then(res => res.json())
       .then(json => {
+        console.log(json)
         setImages(json)
       })
   }, [])
   return (
     <div>
-      Hello Next.js
-      {
-        images.map((image, index) => {
+      <h1>SSR</h1>
+      <div className={styles.container}>
+      { 
+        images?.map((image, index) => {
           return (
-            <img
-              src={image.thumbnailUrl}
-              width={150}
-              height={150}
-            />
+            <div>
+              <div className={styles.images}>
+                <img
+                  src={image.thumbnailUrl}
+                  width={150}
+                  height={150}
+                />
+              </div>
+              <div>{image.title}</div>
+            </div>
           )
         })
       }
-
+      </div>
     </div>
   )
 }
